faa61d012d96a3b8faf97796bc2e0d5ecfa23bf7
Laura <laura.galea@yahoo.com>
2017-05-19 00:39:55 +0100

    String Permutation

---
+++ assigment_1_Q1.py
@@ -0,0 +1,8 @@
+
+def check_permutation(str1, str2):
+    return sorted(str1.lower())==sorted(str2.lower())
+
+
+#print(check_permutation('Listen', 'Silent'))
+#print(check_permutation('pants', 'stamp'))
+#print(check_permutation('2', 'He21'))
