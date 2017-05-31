39b38feb676ac3111bfe363009b8b24c895d515a
galeal2 <laura.galea2@mail.dcu.ie>
2017-05-31 19:34:33 +0100

    used libraries

--- assignment_2_Q2.py
+++ assignment_2_Q2.py
@@ -1,57 +1,5 @@
-class Node:
-    def __init__(self, item, left = None, right = None):
-        self.item = item
-        self.left = left
-        self.right = right
-
-class BST:
-    def __init__(self, lst = None):
-        self.root = None
-        if lst != None:
-            for item in lst:
-                self.add(item)
-
-    def add(self, item):
-        if self.root == None:
-            self.root = Node(item, None, None)
-        else:
-            # Find where to put the item
-            child_tree = self.root
-            while child_tree != None:
-                parent = child_tree
-                if item < child_tree.item: 
-                    child_tree = child_tree.left 
-                else:
-                    child_tree = child_tree.right
-
-            # child_tree should be pointing to the new node, but we've gone too far
-            # we need to modify the parent nodes
-            if item < parent.item:
-                parent.left = Node(item, None, None)
-            else:
-                parent.right = Node(item, None, None)
-
-
-
-# find all ancestors for a Binary Tree
-def rec_find_ancestors(ptr, key, path):
-    if ptr == None:
-        return False
-    if ptr.item == key:
-        return True
-    if rec_find_ancestors(ptr.left, key, path) or rec_find_ancestors(ptr.right, key, path):
-        path += [ptr.item]
-        return path
-    return False
-
-def find_ancestors(BT, key):
-    ptr = BT.root
-    if BT.root.item == key:
-        return [BT.root.item]
-    if not rec_find_ancestors(ptr, key, []):
-        return []
-    return rec_find_ancestors(ptr, key, [])
-
+from BST import BST
+from assignment_2_Q1 import find_ancestors
 
 # find the lowest common ancestor
 def find_common_ancestor(BT, node1, node2):
